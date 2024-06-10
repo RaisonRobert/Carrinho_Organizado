fafca1e9428649c764281218b25b1b6f58373561
Raison Robert Monteiro de Sá <98828337+RaisonRobert@users.noreply.github.com>
2024-06-10 13:51:48 -0400

    Revert "apply mvvm in login"
    This reverts commit f3ee00593a1cb7b3f1ebb7da09cfc2c0507ee33a.

--- app/src/main/java/com/carrinhoorganizado/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/carrinhoorganizado/view/fragment/LoginFragment.kt
@@ -6,20 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.carrinhoorganizado.R
 import com.carrinhoorganizado.databinding.FragmentLoginBinding
 import com.carrinhoorganizado.view.activity.HomeActivity
-import com.carrinhoorganizado.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val mBinding get() = _binding!!
-    private val mViewModel: LoginViewModel by viewModels ()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,40 +28,11 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupOnClick()
-        observeUI()
-    }
-
-    private fun observeUI() {
-        mViewModel.startLogin.observe(viewLifecycleOwner){startLogin->
-            if (startLogin){
-                clearErrorField()
-                mViewModel.startLoginAuthenticationFirebase()
-            }
-        }
-        mViewModel.errorLogin.observe(viewLifecycleOwner){errorLogin->
-            if (errorLogin){
-                errorLogin("Error digite novamente seu login")
-            }
-
-        }
-    }
-
-    private fun clearErrorField() {
-        mBinding.layoutLogin.error = null
-        mBinding.layoutPassword.error = null
-    }
-
-    private fun errorLogin(msg: String) {
-        mBinding.layoutLogin.error = msg
-        mBinding.layoutPassword.error = msg
     }
 
     private fun setupOnClick() {
         mBinding.btnLogin.setOnClickListener {
-//            startActivity(Intent(requireContext(), HomeActivity::class.java))
-            val email = mBinding.fieldLogin.text.toString()
-            val password = mBinding.fieldPassword.text.toString()
-            mViewModel.verifyFieldLogin(email, password)
+            startActivity(Intent(requireContext(), HomeActivity::class.java))
         }
         mBinding.txtCreateAcount.setOnClickListener {
             findNavController().navigate(R.id.action_login_to_create_acount)
